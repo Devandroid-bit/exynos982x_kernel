dcbccb86f5aebd2a326ffda8441ca0ccd0478320
Devandroid-bit <180217512+Devandroid-bit@users.noreply.github.com>
2026-04-17 16:22:40 +0100

    sscope-minimized manual hooks v1.6 (original author: @backslashxx)
    Replaced '#ifdef CONFIG_KSU' with '#if defined CONFIG_KSU' for consistency and clarity.
    
    Signed-off-by: Devandroid-bit <180217512+Devandroid-bit@users.noreply.github.com>

--- fs/stat.c
+++ fs/stat.c
@@ -163,19 +163,12 @@ EXPORT_SYMBOL(vfs_statx_fd);
  *
  * 0 will be returned on success, and a -ve error code if unsuccessful.
  */
-#ifdef CONFIG_KSU
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
-#endif
 int vfs_statx(int dfd, const char __user *filename, int flags,
 	      struct kstat *stat, u32 request_mask)
 {
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
-
-#ifdef CONFIG_KSU
-	ksu_handle_stat(&dfd, &filename, &flags);
-#endif
 
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
@@ -360,7 +353,7 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 	return cp_new_stat(&stat, statbuf);
 }
 
-#ifdef CONFIG_KSU
+#if defined CONFIG_KSU
 __attribute__((hot)) 
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user,
 				int *flags);
@@ -373,7 +366,7 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
-#ifdef CONFIG_KSU
+#if defined CONFIG_KSU
 	ksu_handle_stat(&dfd, &filename, &flag);
 #endif
 
@@ -660,6 +653,10 @@ COMPAT_SYSCALL_DEFINE4(newfstatat, unsigned int, dfd,
 {
 	struct kstat stat;
 	int error;
+
+#if defined(CONFIG_KSU)
+	ksu_handle_stat(&dfd, &filename, &flag); 
+#endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
